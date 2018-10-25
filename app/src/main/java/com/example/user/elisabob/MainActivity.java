f95ffe94ffe2f91921e588f9b855546b13d8bbc1
SpartakChampion <ivan_chepelkin@mail.ru>
2018-10-25 15:47:20 +0300

    Enum

--- app/src/main/java/com/example/user/elisabob/MainActivity.java
+++ app/src/main/java/com/example/user/elisabob/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +12,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView ballsBob;
     private TextView ballsElisa;
     private List<Person> list;
-
-    char E = 'E';
-    char M = 'M';
-    char H = 'H';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,31 +37,22 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     private void searchWhoIsWin(List<Person> people) {
         List<Integer> worksList = new ArrayList<>();
-
         int hours = 0;
         int position = 0;
-
-
         for (int i = 0; i < people.size(); i++) {
-
             Person person = people.get(i);
             String work = person.getWork();
             char[] strWorkToArray = work.toCharArray();
 
             for (int j = 0; j < strWorkToArray.length; j++) {
-                
-
-                if (strWorkToArray[j] == E) {
-                    hours = hours + 1;
-                } else if (strWorkToArray[j] == M) {
-                    hours = hours + 3;
-                } else if (strWorkToArray[j] == H) {
-                    hours = hours + 5;
+                for (SearchHours s : SearchHours.values()) {
+                    if (s.name().equals(String.valueOf(strWorkToArray[j]))) {
+                        hours = s.action(hours);
+                    }
                 }
             }
             worksList.add(hours);
             hours = 0;
-
             if (person.getName().equals("Bob")) {
                 ballsBob.setText(worksList.get(i).toString());
             } else if (person.getName().equals("Elisa")) {
@@ -74,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         int max = worksList.get(0);
-
         for (int i = 0; i <= worksList.size() - 1; i++) {
             if (worksList.get(i) > max) {
                 max = worksList.get(i);
@@ -84,23 +69,22 @@ public class MainActivity extends AppCompatActivity {
         textWin.setText("Победу одержал " + people.get(position).getName());
     }
 
-
-    private enum SearchHours {
-        E {
-            int is() {
-                return 1;
+    enum SearchHours {
+        E() {
+            public int action(int x) {
+                return x + 1;
             }
         },
-
         M {
-            int is() {
-                return 3;
+            public int action(int x) {
+                return x + 3;
             }
         },
         H {
-            int is() {
-                return 1;
+            public int action(int x) {
+                return x + 5;
             }
-        }
+        };
+        public abstract int action(int x);
     }
 }
